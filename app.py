b4f55ed0c3e612a301b707ee794473d580308c50
Emad el Din Ahmed <emadsaghier@gmail.com>
2025-03-14 13:39:51 +0200

    comment

--- app.py
+++ app.py
@@ -164,3 +164,6 @@ def update_tabs(selected_tab):
 
 if __name__ == "__main__":
     app.run_server(debug=True)
+    
+    
+##notes:analytics tab pauses the scrapping process untill further updates
